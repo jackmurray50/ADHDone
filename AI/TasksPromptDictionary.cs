b504289b28b6f4f39bb0ecb9fae61d524d448111
Jack Murray <47582604+jackmurray50@users.noreply.github.com>
2025-09-07 23:18:55 -0400

    Changed accessibility to public

--- AI/TasksPromptDictionary.cs
+++ AI/TasksPromptDictionary.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace ADHDone.AI
 {
-    internal static class TasksPromptDictionary
+    public static class TasksPromptDictionary
     {
-        private static Dictionary<Prompt, string> prompts = new()
+        public static Dictionary<Prompt, string> prompts = new()
         {
             [Prompt.ParseTaskFromUserInput] = "For this task, provide me with a Summary, Description, Category, MinEstimatedCompletionTime, MaxEstimatedCompletionTime, ToFinishDate, LocationType, and Priority. For the priority, determine if the task is Urgent/Important, Not Urgent/Important, Not Important/Urgent, or Not Important/Not Urgent. Respond using 1, 2, 3, or 4 in that order. LocationType should be something like 'home', 'grocery store' or similar. Respond in JSON."
         };
-        enum Prompt
+        public enum Prompt
         {
             ParseTaskFromUserInput
         }
