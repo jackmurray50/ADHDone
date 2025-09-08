cc8329c48bf68b48725b0ce7baee69d388a92ca2
Jack Murray <47582604+jackmurray50@users.noreply.github.com>
2025-09-08 12:24:41 -0400

    Created Priority constructor

--- TaskList/Modules/Priority.cs
+++ TaskList/Modules/Priority.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Color = Microsoft.Maui.Graphics.Color;
 
 namespace ADHDone.TaskList.Modules
 {
@@ -14,6 +16,31 @@ namespace ADHDone.TaskList.Modules
     {
         public int PriorityNumber { get; set; }
         public string PriorityName { get; set; }
-        public Color Color { get; set; }
+        public Microsoft.Maui.Graphics.Color Color { get; set; }
+        internal Priority(int priorityNumber)
+        {
+            PriorityNumber = priorityNumber;
+            switch (priorityNumber)
+            {
+                case 1:
+                    PriorityName = "Urgent/Important";
+                    Color = Colors.Red;
+                    break;
+                case 2:
+                    PriorityName = "Not Urgent/Important";
+                    Color = Colors.Orange;
+                    break;
+                case 3:
+                    PriorityName = "Not Important/Urgent";
+                    Color = Colors.Yellow;
+                    break;
+                case 4:
+                    PriorityName = "Not Important/Not Urgent";
+                    Color = Colors.Green;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("Priority number must be between 1 and 4.");
+            }
+        }
     }
 }
