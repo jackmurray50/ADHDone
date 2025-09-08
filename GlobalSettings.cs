3064f3d050c8a52f8105410aa29b268f920b1e02
Jack Murray <47582604+jackmurray50@users.noreply.github.com>
2025-09-08 12:06:24 -0400

    Changed fields to properties and fixed loading

--- GlobalSettings.cs
+++ GlobalSettings.cs
@@ -10,31 +10,44 @@ namespace ADHDone
 {
     internal class GlobalSettings
     {
-        public string DefaultTask;
+        public string DefaultTask { get; set; }
+        public List<string> Locations { get; set; }
+        public List<string> Categories { get; set; }
 
-        public GlobalSettings()
+        private void LoadDefaults()
         {
-            Load();
-        }
-        public void Load()
-        {
-            try
-            {
-                GlobalSettings gs = JsonSerializer.Deserialize<GlobalSettings>(File.ReadAllText(@"Settings.json"));
-                this.DefaultTask = gs.DefaultTask;
-            }
-            catch
-            {
-                //it was blank; load in defaults
-                DefaultTask = "Default";
-                Save();
-            }
+            DefaultTask = "Default";
+            Locations = new List<string>() { "Home", "Work", "Grocery Store", "Online", "Outdoors" };
+            Categories = new List<string>() { "Chore", "Work", "Personal Development", "Health", "Hobby" };
+            Save();
         }
 
         public void Save()
         {
             string json = JsonSerializer.Serialize(this);
             File.WriteAllText(@"Settings.json", json);
+        }
+
+        public static GlobalSettings RetrieveGlobalSettings() {
+            GlobalSettings? gs;
+            try
+            {
+                string json = File.ReadAllText(@"Settings.json");
+                gs = JsonSerializer.Deserialize<GlobalSettings>(json);
+                if (gs is null)
+                {
+                    //it was blank; load in defaults
+                    gs = new GlobalSettings();
+                    gs.LoadDefaults();
+                }
+            }
+            catch (Exception ex)
+            {
+                //it was blank; load in defaults
+                gs = new GlobalSettings();
+                gs.LoadDefaults();
+            }
+            return gs;
         }
     }
 }
